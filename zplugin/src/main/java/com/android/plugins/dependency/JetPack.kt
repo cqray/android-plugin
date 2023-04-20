177735cf7ecccbadd480f7904a3d9956d80fec1a
PC <soft.leijue@qq.com>
2023-04-20 15:22:20 +0800

    initial commit

--- zplugin/src/main/java/com/android/plugins/dependency/JetPack.kt
+++ zplugin/src/main/java/com/android/plugins/dependency/JetPack.kt
@@ -24,40 +24,20 @@ class JetPack(targetSdkVersion: Int) {
     )
 
     /// Room数据库
-
     val roomRxjava3 get() = "androidx.room:room-rxjava3:$roomVersion"
     val roomRxjava2 get() = "androidx.room:room-rxjava2:$roomVersion"
     val roomRuntime get() = "androidx.room:room-runtime:$roomVersion"
     val roomCoroutine get() = "androidx.room:room-ktx:$roomVersion"
     val roomCompiler get() = "androidx.room:room-compiler:$roomVersion"
 
-    val lifecycleJava8 = "androidx.lifecycle:lifecycle-common-java8:2.4.0"
+    val lifecycleJava8 = "androidx.lifecycle:lifecycle-common-java8:2.5.1"
 
     /// 协程依赖
-
     val coroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2"
-
     /// 协程扩展
-
     val coroutineViewModel get() = "androidx.lifecycle:lifecycle-viewmodel-ktx:$coroutineExtVersion"
     val coroutineRuntime get() = "androidx.lifecycle:lifecycle-runtime-ktx:$coroutineExtVersion"
     val coroutineLivedata get() = "androidx.lifecycle:lifecycle-livedata-ktx:$coroutineExtVersion"
 
     val core get() = "androidx.core:core:1.6.0"
-
-////    // lifecycle对于协程的扩展封装
-////    api 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2'
-////    api "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
-////    api "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
-////    api "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
-//
-//    companion object {
-//
-////        /** Room数据库 **/
-////        private val roomVersion: String = Configuration.createDepVersion("2.3.0", "2.5.1")
-//
-//        /** 协程扩展版本 **/
-//        private val coroutineExtVersion: String get() = Configuration.createDepVersion("2.3.1", "2.5.1")
-//
-//    }
 }
---
+++ zplugin/src/main/java/com/android/plugins/dependency/Smart.kt
@@ -0,0 +1,36 @@
+package com.android.plugins.dependency
+
+/**
+ * https://github.com/scwang90/SmartRefreshLayout （刷新框架）
+ */
+class Smart {
+    
+    private val version = "2.0.5"
+
+    /** (SmartRefreshLayout) 刷新核心依赖库 **///https://github.com/scwang90/SmartRefreshLayout
+    val smartKernel = "com.scwang.smart:refresh-layout-kernel:$version"
+
+    /** (SmartRefreshLayout) 经典刷新头 **/
+    val smartHeaderClassic = "com.scwang.smart:refresh-header-classics:$version"
+
+    /** (SmartRefreshLayout) 雷达刷新头 **/
+    val smartHeaderRadar = "com.scwang.smart:refresh-header-radar:$version"
+
+    /** (SmartRefreshLayout) 虚拟刷新头 **/
+    val smartHeaderFalsify = "com.scwang.smart:refresh-header-falsify:$version"
+
+    /** (SmartRefreshLayout) 谷歌刷新头 **/
+    val smartHeaderMaterial = "com.scwang.smart:refresh-header-material:$version"
+
+    /** (SmartRefreshLayout) 二级刷新头 **/
+    val smartHeaderTwoLevel = "com.scwang.smart:refresh-header-two-level:$version"
+
+    /** (SmartRefreshLayout) 球脉冲加载 **/
+    val smartFooterBall = "com.scwang.smart:refresh-footer-ball:$version"
+
+    /** (SmartRefreshLayout) 经典加载 **/
+    val smartFooterClassic = "com.scwang.smart:refresh-footer-classics:$version"
+
+    /** (SmartRefreshLayout) 横向智能刷新框架 **///https://github.com/scwang90/SmartRefreshHorizontal
+    val smartHorizontal = "com.scwang.smart:refresh-layout-horizontal:2.0.0"
+}
